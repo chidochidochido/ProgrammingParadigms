d548819734ff0194cdf3ea8c5a247c3186e02a25
Marco Ramirez <chidochidochido@gmail.com>
2017-01-26 19:33:13 -0600

    Lecture 4

--- lsearch/lsearch.c
+++ lsearch/lsearch.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-int lsearch(int key, int array[], int size) 
+void* lsearch(void *key, void *base, int n, int elemSize) 
 {
     int i;
 
-    for(i = 0; i < size; i++) 
+    for(i = 0; i < n; i++) 
     {
-       if(array[i] == key) 
-       {
-           return i;
-       }
+       void *elemAddr = (char*)base + i*elemSize;
+       if(memcmp(key, elemAddr, elemSize) == 0)
+           return elemAddr;
     }
-    return -1;
+    return NULL;
 }
 
 main() 
 {
     int array[] = {1, 2, 3, 4, 5};
-    int key = 6;
+    int key = 2;
 
-    printf("%d\n", lsearch(key, array, sizeof(array)/sizeof(int)));
+    printf("%d\n", lsearch(&key, array, sizeof(array)/sizeof(int), sizeof(int)));
 }
